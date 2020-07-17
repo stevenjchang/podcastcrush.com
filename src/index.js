f0ebae9370b9a66e71a82bd100de5965fde35f1f
stevenjchang <stevenjchang@gmail.com>
2020-07-16 19:02:54 -0700

    replace ga with gtm

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import HomePage from "./pages/HomePage";
+import TagManager from "react-gtm-module";
+
+const tagManagerArgs = {
+  gtmId: process.env.REACT_APP_GTM_ID,
+};
+
+TagManager.initialize(tagManagerArgs);
 
 ReactDOM.render(
   <React.StrictMode>
