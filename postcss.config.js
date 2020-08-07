73c488a5e4619b7a0dbe05eb0eaf1d959adeb69c
Steve <stevenjchang@gmail.com>
2020-08-07 16:42:20 -0700

    refactor(purgecss): shrunk CSS size, ready made CSS (#13)
    For production build, added:
    
    purgecss - removes all unused tailwind classes
    cssnano - minifies css

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,21 @@
 const tailwindcss = require("tailwindcss");
+const purgecss = require("@fullhuman/postcss-purgecss");
+const cssnano = require("cssnano");
+
 module.exports = {
-  plugins: [tailwindcss("./tailwind.js"), require("autoprefixer")],
+  plugins: [
+    tailwindcss("./tailwind.js"),
+    process.env.NODE_ENV === "production"
+      ? purgecss({
+          content: ["./src/*.js", "./src/**/*.js", "./src/**/**/*.js"],
+          defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+        })
+      : null,
+    process.env.NODE_ENV === "production" ? require("autoprefixer") : null,
+    process.env.NODE_ENV === "production"
+      ? cssnano({ preset: "default" })
+      : null,
+  ],
 };
+
+//see REFERENCES.md - purgecss + watch + cssnano
