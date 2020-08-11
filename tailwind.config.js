880448d3da5c46d912ad08b67c18239016e54473
Steve <stevenjchang@gmail.com>
2020-08-10 19:06:46 -0700

    fix(purgecss): stripping defaults (#14)

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   theme: {
     extend: {},
   },
+  purge: false, //NOTE: purge is on, but through postcss. This is just to turn off tailwindcss warning message
   variants: {},
   plugins: [],
-}
+};
